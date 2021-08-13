aee3d6389832e0d25ad88ff6a6b8d5eac495642c
Hyongen <feferhsu@gmail.com>
2021-08-13 16:34:31 -0300

    Update

---
+++ CoderHouse/Javascript/javascript/compararNum.js
@@ -0,0 +1,10 @@
+let num1 = prompt("Ingrese el primer número");
+let num2 = prompt("Ingrese el segundo número ");
+
+if (num1 > num2) {
+    alert("El primero número es más grande.");
+} else if (num1 < num2) {
+    alert("El segundo número es más grande.");
+} else if (num1 == num2) {
+    alert("Ambos números son iguales");
+}
